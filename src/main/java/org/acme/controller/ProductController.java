38dc2e7ae992d0818c709f8e1a3552649ba92e70
Augusto <augustojbe@gmail.com>
2023-06-23 09:24:23 -0300

    first commit

---
+++ src/main/java/org/acme/controller/ProductController.java
@@ -0,0 +1,68 @@
+package org.acme.controller;
+
+
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.acme.dto.ProductDTO;
+import org.acme.service.ProductService;
+
+import java.util.List;
+
+@Path("/api/products")
+public class ProductController {
+
+    @Inject
+    ProductService productService;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<ProductDTO> findAllProducts(){
+        return productService.getAllProducts();
+    }
+
+    @POST
+    @Transactional
+    public Response saveProduct(ProductDTO product){
+        try {
+            productService.createNewProduct(product);
+            return Response.ok().build();
+        } catch (Exception e){
+            e.printStackTrace();
+            return Response.serverError().build();
+        }
+
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Transactional
+    public Response changeProduct(@PathParam("id") Long id, ProductDTO product){
+        try {
+            productService.changeProduct(id, product);
+            return Response.ok().build();
+        } catch (Exception e){
+            e.printStackTrace();
+            return Response.serverError().build();
+        }
+
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Transactional
+    public Response removeProduct(@PathParam("id") Long id){
+        try {
+            productService.deleteProduct(id);
+            return Response.ok().build();
+        } catch (Exception e){
+            e.printStackTrace();
+            return Response.serverError().build();
+        }
+
+    }
+
+
+}
